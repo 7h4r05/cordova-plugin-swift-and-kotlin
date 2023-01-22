d0e19bf63d91aa070ab8ee548b88cbe07e91b9e2
Dariusz <dariusz@ridely.com>
2023-01-22 09:54:29 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cordova-plugin-swift-and-kotlin',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
